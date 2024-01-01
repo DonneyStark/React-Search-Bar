65e108bc6d266d9df707185c9dc28f4d05e2ea5e
Metha Dondee <methadondee@Methas-MacBook-Pro.local>
2024-01-02 02:23:17 +0700

    first commit

---
+++ src/data/tattoos.js
@@ -0,0 +1,35 @@
+const tattoos = [
+  {
+    title: "สักแขน",
+    thumbnail: "/images/tattoo-01-small.jpg",
+    full: "/images/tattoo-01-large.jpg",
+  },
+  {
+    title: "สักมือ",
+    thumbnail: "/images/tattoo-02-small.jpg",
+    full: "/images/tattoo-02-large.jpg",
+  },
+  {
+    title: "สักคอ",
+    thumbnail: "/images/tattoo-03-small.jpg",
+    full: "/images/tattoo-03-large.jpg",
+  },
+  {
+    title: "สักหลัง",
+    thumbnail: "/images/tattoo-04-small.jpg",
+    full: "/images/tattoo-04-large.jpg",
+  },
+  {
+    title: "สักแขน",
+    thumbnail: "/images/tattoo-05-small.jpg",
+    full: "/images/tattoo-05-large.jpg",
+  },
+  {
+    title: "สักแขน",
+    thumbnail: "/images/tattoo-06-small.jpg",
+    full: "/images/tattoo-06-large.jpg",
+  },
+  
+];
+
+export default tattoos;
